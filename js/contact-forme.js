7c95f0f4b127ad0fc7bcd121113e8357dbeffe8b
Vitalii Shtypuk <vitaliishtypuk@gmail.com>
2017-12-22 13:06:21 +0000

    Initial

--- js/contact-forme.js
+++ js/contact-forme.js
@@ -1,22 +1,23 @@
-$(function() {
-    $('#contact-form input[type=submit]').click(sendForm);
+$(function(){
+   $('#contact-form input[type=submit]').click(sendForm); 
 });
 
 function sendForm(ev) {
-    const form = document.getElementsByTagName('form')[0];
+    const form  = document.getElementsByTagName('form')[0];
     if (form.checkValidity()) {
         ev.preventDefault();
         $.ajax({
-                url: "https://formspree.io/vitaliishtypuk@gmail.com",
-                method: "POST",
-                data: {
-                    name: $('#client-name').val(),
-                    email: $('#client-email').val(),
-                    comment: $('#comment').val()
-                },
-                dataType: "json"
-            })
-            .done(() => $('#thank-dialog').attr('open', 'close'))
-            .fail(() => $('#error-dialog').attr('open', 'close'));
+            url: "https://formspree.io/vitaliishtypuk@gmail.com", 
+            method: "POST",
+            data: {
+                name: $('#inputname').val(),
+                email: $('#inputemail').val(),
+                tel:$('#inputtel').val(),
+                feedback: $('#feedback').val()
+            },
+            dataType: "json"
+        })
+         .done( () => $('#thank-dialog-close').removeClass('close') )
+         .fail( () => $('#error-dialog-close').removeClass('close') ); 
     }
 }
